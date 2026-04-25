12793c75c338e380557a60bf4971e611782c1235
cybera1chemist <1849770138@qq.com>
2026-04-25 14:35:49 +0800

    Built a simple shop building environment

--- Assets/Scenes/Shop/ShopItem.cs
+++ Assets/Scenes/Shop/ShopItem.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ShopItem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("References")]
+    [SerializeField] private GameObject ItemInfoQuad;
+
     void Start()
     {
-        
+        ItemInfoQuad.SetActive(false); // Hide the item info quad at the start
     }
 
     // Update is called once per frame
@@ -15,6 +17,19 @@ public class ShopItem : MonoBehaviour
     {
         
     }
+
+    #region Icon
+    public void OnIconHovered()
+    {
+        Debug.Log("Icon hovered");
+        ItemInfoQuad.SetActive(true); // Show the item info quad when hovered
+    }
+    public void OnIconHoverExit()
+    {
+        Debug.Log("Icon hover exited");
+        ItemInfoQuad.SetActive(false); // Hide the item info quad when hover exits
+    }
+    #endregion
 
     public void OnBuyButtonClicked(){
         Debug.Log("Buy button clicked");
